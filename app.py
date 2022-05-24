cca87b39c040c08f7c0bf625b4df97564ca11d2e
ghdrl858 <ghdrl858@gmail.com>
2022-05-24 12:37:20 +0900

    2022-05-24

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from fbprophet import Prophet
+
+def main() :
+    pass
+
+if __name__ == '__main__' :
+    main()
+
+## 파이썬 가상환경 확인
+## 이 앱은 구글 코랩에서 만든 아보카도 가격 예측 환경과 동일하게 하기 위해서 
+## 파이썬 가상환경을 strealit3.7로 바꿔준다.
+## -> vs code 오른쪽 아래에 base -> streamlit3.7로 변경
